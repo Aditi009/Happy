5aba425f4c206aa042efb09063256a45ee74eaf5
Aditi009 <55443683+Aditi009@users.noreply.github.com>
2020-01-29 20:36:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,123 @@
+<html>
+<head>
+	<style>
+		#black
+		{
+			height:80px;
+			width:80;
+			background:black;
+			float:left;
+		}
+		#white
+		{
+			height:80px;
+			width:80px;
+			background:white;
+			float:left;
+		}
+	</style></head>
+<body align="center">
+	<h1>Chess Board</h1>
+	<table border=".1">
+	  <tr>
+	      <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  
+		  		  
+
+	 </tr>
+	<tr>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		
+	</tr>
+	<tr>
+	      <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  
+		  		  
+
+	 </tr>
+	<tr>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		
+	</tr>
+	<tr>
+	      <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  
+		  		  
+
+	 </tr>
+	<tr>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		
+	</tr>
+	<tr>
+	      <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  <td><div id="black"></div></td>
+		  <td><div id="white"></div></td>
+		  
+		  		  
+
+	 </tr>
+	<tr>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		<td><div id="white"></div></td>
+		<td><div id="black"></div></td>
+		
+	</tr>
+	
+	</table>
+	
+	<div id="white"></div>
+
+	</body></html>
